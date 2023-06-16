f254fc395474c8154841af97ea24df7416d8cefa
matiasmza1 <matiasmza1@gmail.com>
2023-06-15 22:48:34 -0300

    icono en navbar

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -12,7 +12,7 @@ export class NavbarComponent {
 
   constructor(){
     this.titulo = 'INCUYO';
-    this.imagen = 'https://scontent.fmdz4-1.fna.fbcdn.net/v/t39.30808-6/310373582_495586489245996_7176748967072740212_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=rDWZeLg0cToAX-wDsIS&_nc_ht=scontent.fmdz4-1.fna&oh=00_AfDaKHyPS2FmX0KAd6envp3OiEJWXhkBRBI1aveLBTlTNA&oe=64869512';
+    this.imagen = 'https://scontent.fmdz11-1.fna.fbcdn.net/v/t39.30808-6/310373582_495586489245996_7176748967072740212_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE9t83YESjxvyjiukm-sRDNPlHv1SiToAM-Ue_VKJOgA_FACKyddOTcdqcAr3puWig&_nc_ohc=JV_kM_Yy3uoAX88NoV1&_nc_ht=scontent.fmdz11-1.fna&oh=00_AfDvS-vUR8rWPsleSc2zeQ_MUuqNXUWm5a3nXBVfhc7TnA&oe=64907852';
 
   }
 
